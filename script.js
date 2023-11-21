aa1183b6a4bdfbfe345e805b2040aaf0f4c8b554
Sufiyan AK <Sufiyankhattak14@gmail.com>
2023-11-21 12:25:04 +0500

    Fixed some bugs on generating html and make list btns interactive

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 const todoTask = JSON.parse(localStorage.getItem('todoTask')) || [];
 
-let todoTaskHTML = '';
-
-todoTask.forEach(value => {
-    todoTaskHTML += 
-    `
-    <p class="task-type">${value}</p>
-    <div class="btns">
-        <p class="btn edit-btn"><i class="bi bi-exposure"></i></p>
-        <p class="btn delete-btn"><i class="bi bi-trash"></i></p>
-    </div>
-    `;
-})
-
-document.querySelector('.render-task').innerHTML = todoTaskHTML;
+function renderTodoList() {
+    let todoTaskHTML = '';
+    todoTask.forEach((value, index)=> {
+        const {task} = value;
+        todoTaskHTML += 
+        `
+        <div>
+            <p class="task-type">${task}</p>
+            <div class="btns">
+                <p class="btn edit-btn"><i class="bi bi-exposure"></i></p>
+                <p class="btn delete-btn" onclick="deleteTodo(${index})"><i class="bi bi-trash"></i></p>
+            </div>
+        </div>
+        `;
+    })
+    document.querySelector('.render-task').innerHTML = todoTaskHTML;
+}
 
 const task =  document.querySelector('#todo-task');
 
@@ -23,31 +26,35 @@ addBtn.addEventListener('click', () => {
     saveToStorage();
 });
 
-const editBtn = document.querySelector('.edit-btn');
-editBtn.addEventListener('click', () => {
-    console.log('edit Btn')
-})
-
-const deleteBtn = document.querySelector('.delete-btn');
-deleteBtn.addEventListener('click', () => {
-    console.log('delete Btn')
-})
-
 task.addEventListener('keydown', (event) => {
     if(event.code === 'Enter') {
-        addTask(task);
-        saveToStorage();
-        console.log(event);
+        addTask();
     }
 })
 
+function addTask() {
+    let value = task.value;
 
-function addTask(task) {
-    task = task.value;
-    todoTask.push(task);
-    console.log(todoTask);
+    if (value.trim() === '') {
+        return;
+    }
+
+    todoTask.push({value});
+
+    task.value = '';
+
+    renderTodoList();
+
+    saveToStorage();
+}
+
+function deleteTodo(index) {
+    todoTask.splice(index, 1);
+    renderTodoList();
 }
 
 function saveToStorage() {
     localStorage.setItem('todoTask',JSON.stringify(todoTask));
 }
+
+renderTodoList();
